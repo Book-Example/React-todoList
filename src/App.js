1f39fb2327e8add8555b34277e7b9eb83052eb70
iinsue <hinsu93@naver.com>
2022-03-17 15:48:42 +0900

    10.3.3 | 지우기 기능 구현하기(281p)

--- src/App.js
+++ src/App.js
@@ -38,12 +38,17 @@ const App = () => {
     },[todos],
   )
 
+  const onRemove = useCallback(
+    id => {
+      setTodos(todos.filter(todo => todo.id !== id));
+    },[todos],
+  );
 
   return (
    <div>
      <TodoTemplate>
      <TodoInsert onInsert={onInsert}/>
-     <TodoList todos={todos}/>
+     <TodoList todos={todos} onRemove={onRemove}/>
      </TodoTemplate>
    </div>
   );
